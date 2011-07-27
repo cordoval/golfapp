4fab70dc3d55beeb5e55bea38e2122fcd231dff5
Seb Armand <khepin@gmail.com>
2011-07-27 15:44:36 +0800

    Golf course entity

---
+++ src/Khepin/GolfBundle/Entity/Course.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Khepin\GolfBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Khepin\GolfBundle\Entity\Course
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Khepin\GolfBundle\Entity\CourseRepository")
+ */
+class Course
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $city
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var integer $holes
+     *
+     * @ORM\Column(name="holes", type="integer")
+     */
+    private $holes;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set holes
+     *
+     * @param integer $holes
+     */
+    public function setHoles($holes)
+    {
+        $this->holes = $holes;
+    }
+
+    /**
+     * Get holes
+     *
+     * @return integer 
+     */
+    public function getHoles()
+    {
+        return $this->holes;
+    }
+}
