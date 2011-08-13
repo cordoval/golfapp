e67f18d55821142ce359b1c5d2aed3be45f519c5
Seb Armand <khepin@gmail.com>
2011-08-13 15:07:21 +0800

    update forms to not use DI in the constructor but through 'data' option. ParSetType might still need an update

--- src/Khepin/GolfBundle/Form/ParSetType.php
+++ src/Khepin/GolfBundle/Form/ParSetType.php
@@ -4,33 +4,27 @@ namespace Khepin\GolfBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
-use Khepin\GolfBundle\Entity\Course;
 use Khepin\GolfBundle\Form\HoleType;
 use Khepin\GolfBundle\Entity\Hole;
 
 class ParSetType extends AbstractType
 {
-    /**
-     *
-     * @var Course
-     */
     private $course;
     
-    public function __construct(Course $course) {
+    public function __construct(\Khepin\GolfBundle\Entity\Course $course){
         $this->course = $course;
     }
     
     public function buildForm(FormBuilder $builder, array $options)
     {
-        $builder->add('course', 'hidden', array('data' => $this->course->getId()));
-        $builder->add('holes', 'collection', array(
-            'type' => new HoleType(),
-        ));
+        // CollectionType always adds a csrf. We don't need it here as it is already
+        // in the ParSetType. Keeping it keeps it in the data and we only want Holes in the data
+        $builder->add('holes', 'collection', array('csrf_protection' => false));
         for($i = 1; $i <= $this->course->getHolesNumber(); $i++) {
             $hole = new Hole();
-            $hole->setCourse($this->course);
             $hole->setNumber($i);
-            $builder->get('holes')->add('hole_'.$i, new HoleType($hole));
+            $hole->setCourse($this->course);
+            $builder->get('holes')->add('hole_'.$i, new HoleType(), array('data' => $hole));
         }
     }
 
@@ -38,5 +32,4 @@ class ParSetType extends AbstractType
     {
         return 'khepin_golfbundle_parsettype';
     }
-    
 }
